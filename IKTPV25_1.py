df226731eb626801801c1f800165b42fffce6c8b
opilane <opilane@HP-CZC2349HTB>
2025-10-01 11:08:57 +0300

    harjutus 1. Ülesannded 1.2

--- IKTPV25_1.py
+++ IKTPV25_1.py
@@ -1,1 +1,26 @@
-print("Tere maailm!")
+
+
+# 2.
+#Mis tüüpi on järgnevad muutujad:
+
+# a) vanus = 18
+# b) eesnimi = "Jaak"
+# c) pikkus = 1.65
+# d) kas_käib_koolis = True
+# Mis võimalus veel peale True oleks viimast muutujat väärtustada? Kuidas võiks nende muutujate väärtusi koodis kontrollida?
+# kood tüüpide kontrollimiseks.
+vanus = 18 #int
+eesnimi = "Jaak" #str
+pikkus = 1.65 #float
+kas_käib_koolis = True #bool
+
+print(f"vanus {vanus} on: {type(vanus)}")
+print(f"eesnimi {eesnimi} on: {type(eesnimi)}")
+print(f"pikkus {pikkus} on: {type(pikkus)}")
+print(f"kas_käib_koolis {kas_käib_koolis} on: {type(kas_käib_koolis)}")
+
+#1.Kirjuta enda esimene programm, mis väljastab käsureale teksti: “Tere, maailm!”. 
+#Küsi kasutaja nimi ja muuda tekst, et ta näeks välja nii: “Tere, maailm! Tervitan sind Mati”, kui kasutaja nimi on Mati.
+#Küsi kasutajalt sisend tema vanuse kohta ning väljasta see ekraanile:
+#“Tere, maailm! Tervitan sind Mati! Sa oled N aastat vana.”
+
